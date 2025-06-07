7dfb787f96ffb985e97bcbbee999c3bb1832f650
Uroosa GMD <uroosagmd@gmail.com>
2025-06-07 17:26:58 +0500

    Add files via upload

---
+++ JS Assignment 02/app.js
@@ -0,0 +1,242 @@
+                          
+                          //.......Excercise : 03......//
+
+                        //   Variable For Numbers    //
+//  Question 1  //
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// var age="I am 15 years old";
+// alert(age);
+
+// Question 2  //
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+// var visited="You have visited this site 14 times";
+// alert(visited);
+
+// Question 3  //
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// var birthYear="My Birth year is 2005";
+// alert( birthYear+"\nData type of my declared variables is number ");
+
+// Question 4+  //
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// var VisitorName="John Doe";
+// var ProducTittle="T-shirt(s)";
+// var Quantity="5";
+// document.write(VisitorName+"ordered"+ProducTittle+"on xyz clothing store"+Quantity);
+
+                             //....... End.......//
+
+
+                        //.........Excercise : 04.......//
+
+                        //   Variables Name :LEGAL & ILLEGALE    //
+
+ // Question 1 //
+
+//  1. Declare 3 variables in one statement.
+// var subject = "Math", mark = "85",grade = "A"
+// console.log(subject,mark,grade);
+
+// Question 2 //
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// Legale
+// 1. var name=
+// 2. var lastName=
+// 3. var full_Name=
+// 4. var name22=
+// 5. var $score=
+
+// Illegale
+// 1. var var=
+// 2. var let=
+// 3. var 10name=
+// 4. var your name=
+// 5. var my-name=
+
+// Question 3 //
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+//  document.write("Rules for naming JS variables" + "<br>")
+//  document.write("Variable names can only contain, numbers,$ and  FOr example :$my_first        varaible "  + "<br>")
+//  document.write(" Variables must begin with a letter, $ or  For example $name, _name or name"  + "<br>")
+//  document.write(" Variable names are case Sensitive"  + "<br>")
+//  document.write("Variable names should not be JS Keywords")
+
+
+                                      //........End......//
+
+
+                                    //......Excercise : 05.......//
+
+                                    //     Math Expression    //
+
+// Question 1 //
+
+// 1. Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// document.write("sum of" + num1 + "and" + num2 + "is" + sum);
+
+// Question 2 //
+
+// 2. Repeat task1 for subtraction, multiplication, division &
+// modulus.
+// Subtraction
+// var num1 = 3;
+// var num2 = 5;
+// var sub = num1 - num2;
+// document.write("subtraction of" + num1 + "and" + num2 + "is" + sub +"<br>");
+// // Multiplication
+// var num1 = 3;
+// var num2 = 5;
+// var mul = num1 * num2;
+// document.write("multiplication of" + num1 + "and" + num2 + "is" + mul +"<br>");
+// // Division
+// var num1 = 3;
+// var num2 = 5;
+// var div = num1 / num2;
+// document.write("divsion of" + num1 + "and" + num2 + "is" + div + "<br>");
+// // Modules
+// var num1 = 3;
+// var num2 = 5;
+// var mod = num1 % num2;
+// document.write("modules of" + num1 + "and" + num2 + "is" + mod + "<br>");
+
+// Question 3 //
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// var num =
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// var num ;
+// document.write("Value after variable declaration is" + num)
+// c. Initialize the variable with some number.
+// var num = 50;
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// var num = "Initial value 5";
+// document.write("Initial value 5" + num);
+// e. Increment the variable.
+// var num = 0;
+// alert(num)
+// num = (num + 1)
+// alert(num)
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// var num = 5;
+// num = (num + 1)
+// document.write("Value after increment is:" + num);
+// g. Add 7 to the variable.
+// var num = 6;
+// num = num + 7
+// document.write("value after addition is" + num);
+// i. Decrement the variable.
+// var num = 13;
+// num = num - 1;
+// document.write("value after decrement is:" + num);
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// var num = 9;
+// num = num % 3;
+// document.write("The reminder is:" + num);
+
+// Question 4//
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+//  var ticketPrice = 600;
+// var numberOfTickets = 5;
+// var totalCost = ticketPrice * numberOfTickets;
+// 
+// document.write("Total cost to buy " + numberOfTickets + " tickets to a movie is" + totalCost + " PKR");
+
+// Question  5//
+
+// 5. Write a script to display multiplication table of any
+// number in your browser.
+// var num = 5;
+// document.write("Table of" +num + "<br>"); 
+// var num1 = 4;
+// var num2 = 1;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 2;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 3;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 4;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>")
+// ;var num1 = 4;
+// var num2 = 5;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 6;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 7;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 8;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 9;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+// var num1 = 4;
+// var num2 = 10;
+// var num3 = num1 * num2;
+// document.write(num1 + "×" + num2 + "=" + num3 + "<br>");
+
+// Question 6 //
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+var tem = c f-32*5/9;
+document.write(tem);
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
